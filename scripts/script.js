6424e9fad204f02420003b774542f937913fca7e
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-06-25 23:14:26 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function init() {
     renderLeftSide();
+    renderRightSide();
 }
 
 function renderLeftSide() {
@@ -9,3 +10,14 @@ function renderLeftSide() {
 function getCard() {
     document.getElementById('cards').innerHTML = getCards();
 }
+
+function renderRightSide() {
+    document.getElementById('explore').innerHTML = getExploreSection();
+}
+
+const button = document.getElementById('openDialog');
+    const dialog = document.getElementById('meinDialog');
+
+    button.addEventListener('click', () => {
+        dialog.showModal(); // Öffnet das Fenster
+    });
